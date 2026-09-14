9b2ccc33bb45ef63038fe19dd2a3133bcc0827a2
Wun-Sin You <152066727+StephanieYOU125@users.noreply.github.com>
2026-09-15 06:29:54 +0800

    Refresh cache for language tabs

--- sw.js
+++ sw.js
@@ -1,12 +1,27 @@
-
-const CACHE = "speak-clear-v1";
+const CACHE = "speak-clear-v2";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
