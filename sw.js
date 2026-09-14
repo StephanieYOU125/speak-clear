8b7899574c92b95605c67d4e95ad50e6eadcca27
Wun-Sin You <152066727+StephanieYOU125@users.noreply.github.com>
2026-09-15 06:39:44 +0800

    Refresh cache for audience examples

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "speak-clear-v3";
+const CACHE = "speak-clear-v4";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
