74563619245ddb15f05ea3c13b50122c0adb2460
Wun-Sin You <152066727+StephanieYOU125@users.noreply.github.com>
2026-09-15 06:50:19 +0800

    Refresh cache for audience rules update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "speak-clear-v5";
+const CACHE = "speak-clear-v6";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
