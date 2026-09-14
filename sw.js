ce401c48668194e3e04e5ccc8d22c8623fe4b6b8
Wun-Sin You <152066727+StephanieYOU125@users.noreply.github.com>
2026-09-15 06:58:32 +0800

    Refresh cache for flexible practice update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "speak-clear-v6";
+const CACHE = "speak-clear-v7";
 const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
